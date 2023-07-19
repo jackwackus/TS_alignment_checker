f4fe7bd846ff3a0a41215f068088612d3480bda1
Jack Connor <jconnor@baaqmd.gov>
2023-07-18 17:23:13 -0700

    Prepare Repository for Github Upload
    Finish commenting ts_alignment_functions.R, delete extraneous files, add
    tool_tester.R
     On branch master
     Changes to be committed:
    	deleted:    TS_alignment_checker.R
    	new file:   tool_tester.R
    	modified:   ts_alignment_functions.R
    	deleted:    ts_alignment_functions_archive.R

---
+++ tool_tester.R
@@ -0,0 +1,29 @@
+library(ggplot2)
+library(scales)
+library(plotly)
+library(pracma)
+library(modules)
+
+module_dir = 'C:/Users/jconnor/OneDrive - Bay Area Air Quality Management District/R/TS_alignment_checker'
+setwd(module_dir)
+m = use('ts_alignment_functions.R')
+
+setwd('C:/Users/jconnor/Bay Area Air Quality Management District/Charity Garland - AMSP_shared/Projects/_Mobile Monitoring/2021_Richmond_NorthRichmond_SanPablo_CAMP/_Data/Raw_Data')
+data = read.csv('20220401/_QC_Data/2022-04-01_RSP_NorthRichmond_V1_3.csv')
+data$date <- as.POSIXct(data$datetime, format = "%Y-%m-%d %H:%M:%S", tz = "Etc/GMT+8")
+parameters = c("CO", "CO2_dry", "NOx", "Benzene", "BC", "PM10", "FMPS_TotConc")
+peaks = m$find.common.peaks(data, parameters, 30, 10)
+fig = m$plot.time.bounded.stacked.subplots(data, parameters, start_time, end_time)
+
+"
+Good Example Days:
+20211206
+20211210
+20211214 11:56:28
+20220113
+20220131
+20220204
+2022-02-14 14:24:12
+Examples of multiple peaks within window:
+20220323
+"
--- ts_alignment_functions.R
+++ ts_alignment_functions.R
@@ -193,54 +193,78 @@ find.common.peaks <- function(data, parameters, search_peak_n, return_peak_n)
 {
 	"
 	Runs find.peaks() on all parameters in the parameters vector.
-	Reads through peaks output and identifies all peaks that are shared by at least three parameters.
+	Reads through peak output and identifies all peaks that are shared by at least three parameters.
 	Args:
 		data (dataframe): dataset to look for common peaks in
 		parameters (character vector): parameters to look for common peaks across
-		search_peak_n (integer): for find.peaks() - total number of peaks to search for
-		return_peak_n (integer): for find.peaks() - number of peaks to include in the final matrix
+		search_peak_n (integer): for find.peaks() -> total number of peaks to search for
+		return_peak_n (integer): for find.peaks() -> number of peaks to include in the final matrix
 	Returns:
 		results (dataframe): table containing all the peak indices where common peaks (shared by at least 3 parameters) are found
 			indicates which parameters the peak was found in
 			ordered by peak index	
 	"
+	# Run find.peaks() on all parameters and store peak matrices in a list.
 	matrix_list = list()
 	for (i in 1:length(parameters))
 	{
 		peak_matrix = find.peaks(data, parameters[i], search_peak_n, return_peak_n) 
 		matrix_list = append(matrix_list, list(peak_matrix))
 	}
+
+	# Inform the user that common peak search is commencing.
 	message('Finding common peaks.')
+
+	# Prepare data structures for common peak search.
+	
+	# Create a vector to store shared peak indices.
 	shared_peaks = c()
+	# Create a list containing vectors for each parameter. These vectors start as empty but when filled will...
+	# indicate which peaks in the shared_peaks vector are exhibited by each parameter.	
 	peak_occurrence_table = list()
 	for (element in parameters)
 	{
+		# Begin by creating empty vectors for each parameter.
 		peak_occurrence_table = append(peak_occurrence_table, list(c()))
 	}
+
+
+	# Begin the common peak search. Iterate through the peak matrices for all parameters in the matrix list.
 	for (i in 1:length(matrix_list))
 	{
+		# Pull the vector containing the found peak indices for the parameter.
 		peak_indices = matrix_list[[i]][,1]
+		# Iterate through the parameter's found peak indices.
 		for (peak_index in peak_indices)
 		{
-			peak_occurrence_vector = c()
+			# If the peak index is close to any of the shared peaks that have already been handled,
+			# skip the following routines.
 			if (!proximity.checker(peak_index, shared_peaks, 10))
 			{
+				# Create a vector used for logging if the peak is shared by other parameters.
+				peak_occurrence_vector = c()
+				# Iterate through the peak matrices for all parameters.
 				for (element in matrix_list)
 				{
+					# Check if the peak is close to any of the peaks found for the other parameter.
 					peak_compare_array = element[,1]
 					if (proximity.checker(peak_index, peak_compare_array, 10))
 					{
+						# If a match is found, add a 1 to the peak_occurence_vector.
 						peak_occurrence_vector = append(peak_occurrence_vector, c(1))
 					}
 					else
 					{
+						# If no match is found, add a 0 to the peak_occurence_vector.
 						peak_occurrence_vector = append(peak_occurrence_vector, c(0))
 					}
 				}	
-				#print(peak_occurrence_vector)
+				# If the peak is shared by at least three parameters:
 				if (sum(peak_occurrence_vector) >= 3)
 				{
+					# Add the peak index to the shared_peaks vector,
 					shared_peaks = append(shared_peaks, c(peak_index))
+					# Add the peak occurence information to the peak_occurence_table.
 					for (k in 1:length(peak_occurrence_vector))
 					{
 						peak_occurrence_table[[k]] = append(peak_occurrence_table[[k]], c(peak_occurrence_vector[k]))
@@ -252,15 +276,35 @@ find.common.peaks <- function(data, parameters, search_peak_n, return_peak_n)
 
 		}
 	}
+	
+	"
+	At this stage we have a peak occurence table containing rows for each shared peak.
+	Each row expresses with a 1 or a 0 whether or not the peak is present for each compound.
+	We also have a shared peaks vector that contains the indices of each shared peak. 
+	Each peak in the shared peaks vector corresponds to a row in the peak occurence table. 
+	The shared peaks vector and the peak occurence table have the same peak ordering.
+	"
+	# Convert peak_occurrence_table and shared_peaks into dataframes.
 	peak_occurrence_df = as.data.frame(peak_occurrence_table, col.names = parameters)
 	shared_peaks_df = as.data.frame(list(shared_peaks), col.names = c('Peak Index'))
+	# Create a dataframe containing peak times.
 	shared_peak_times_df = as.data.frame(list(data$date[shared_peaks]), col.names= c('DateTime'))
+	# Combine the three dataframes into a single dataframe and order the rows by peak index.
 	results = cbind(shared_peaks_df, shared_peak_times_df, peak_occurrence_df)
 	return(results[order(results[,1]),])
 }
 
 plot <- function(data, parameter)
 {
+	"
+	Plots a time series of a user specified parameter.
+	Args:
+		data (dataframe): dataframe containing data to plot
+			must have datetimes in POSIXct contained in a column labeled 'date'
+		parameter (character): column name of parameter to plot on y-axis
+	Returns:
+		p (plotly): plotly time series plot
+	"
 	p = ggplot(data) + ggtitle("Test") +
 		labs(y=parameter) + labs(x="Time PST") +
 		geom_line(aes(date, !! sym(parameter))) +
@@ -271,6 +315,17 @@ plot <- function(data, parameter)
 
 plot.time.bounded <- function(data, parameter, start_time, end_time)
 {
+	"
+	Plots a time series of a user specified parameter bounded by user specified start and end times.
+	Args:
+		data (dataframe): dataframe containing data to plot
+			must have datetimes in POSIXct contained in a column labeled 'date'
+		parameter (character): column name of parameter to plot on y-axis
+		start_time (character): start time in format '%H:%M:%S'
+		end_time (character): end time in format '%H:%M:%S'
+	Returns:
+		p (plotly): plotly time series plot
+	"
 	date_string = strftime(data$date[1], "%Y-%m-%d")
 	start_ts = strptime(paste(date_string, start_time), "%Y-%m-%d %H:%M:%S", tz = "Etc/GMT+8")
 	end_ts = strptime(paste(date_string, end_time), "%Y-%m-%d %H:%M:%S", tz = "Etc/GMT+8")
@@ -286,6 +341,15 @@ plot.time.bounded <- function(data, parameter, start_time, end_time)
 
 plot.stacked.subplots <- function(data, parameters)
 {
+	"
+	Plots a stacked set of time series of user specified parameters, with a subplot for each parameter.
+	Args:
+		data (dataframe): dataframe containing data to plot
+			must have datetimes in POSIXct contained in a column labeled 'date'
+		parameters (character vector): vector of column names of parameters to plot on y-axes of subplots
+	Returns:
+		fig (plotly): figure containing stacked time series subplots for each parameter
+	"
 	plot_list = list()
 	for (i in 1:length(parameters))
 	{
@@ -297,6 +361,18 @@ plot.stacked.subplots <- function(data, parameters)
 
 plot.time.bounded.stacked.subplots <- function(data, parameters, start_time, end_time)
 {
+	"
+	Plots a stacked set of time series of user specified parameters bounded by user specified start and end times,
+	with a subplot for each parameter.
+	Args:
+		data (dataframe): dataframe containing data to plot
+			must have datetimes in POSIXct contained in a column labeled 'date'
+		parameters (character vector): vector of column names of parameters to plot on y-axes of subplots
+		start_time (character): start time in format '%H:%M:%S'
+		end_time (character): end time in format '%H:%M:%S'
+	Returns:
+		fig (plotly): figure containing stacked time series subplots for each parameter
+	"
 	plot_list = list()
 	for (i in 1:length(parameters))
 	{
